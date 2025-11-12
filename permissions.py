a4f5687ef41a25cad5fd589c36c2bc2a93be71b0
Carter Plenge <124809586+CarterPlenge@users.noreply.github.com>
2025-11-12 10:37:22 -0600

    Fix typos in permissions.py docstring

--- permissions.py
+++ permissions.py
@@ -1,6 +1,6 @@
 """
-Commands can be configured from discord server settings.
-Not recommended you used this unless you have a specific need. 
+Commands can be configured from Discord server settings.
+I wouldn't recommended you use this unless you have a specific need. 
 """
 
 from discord import app_commands, Interaction
